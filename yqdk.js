7875b98288307cf74ec44e11aa02acf8ffd4e003
siubacy <58424019+siubacy@users.noreply.github.com>
2021-02-13 15:01:00 +0800

    Update yqdk.js

--- yqdk.js
+++ yqdk.js
@@ -41,11 +41,12 @@ function nodePostGetRequest(HOST, PORT, method, callBackFunction, path, cookie) 
         });
     });
     //req.write("bodyString");
-    req.write("province=%E6%B1%9F%E8%A5%BF%E7%9C%81&city=%E8%B5%A3%E5%B7%9E%E5%B8%82&district=%E7%AB%A0%E8%B4%A1%E5%8C%BA&street=%E7%AB%A0%E6%B1%9F%E5%8D%97%E5%A4%A7%E9%81%93&xszt=0&jkzk=0&jkzkxq=&sfgl=1&gldd=&mqtw=0&mqtwxq=&zddlwz=%E6%B1%9F%E8%A5%BF%E7%9C%81%E8%B5%A3%E5%B7%9E%E5%B8%82%E7%AB%A0%E8%B4%A1%E5%8C%BA&sddlwz=&bprovince=%E6%B1%9F%E8%A5%BF%E7%9C%81&bcity=%E8%B5%A3%E5%B7%9E%E5%B8%82&bdistrict=%E7%AB%A0%E8%B4%A1%E5%8C%BA&bstreet=%E7%AB%A0%E6%B1%9F%E5%8D%97%E5%A4%A7%E9%81%93&sprovince=%E6%B1%9F%E8%A5%BF%E7%9C%81&scity=%E8%B5%A3%E5%B7%9E%E5%B8%82&sdistrict=%E7%AB%A0%E8%B4%A1%E5%8C%BA&lng=114.93590908&lat=25.84529554&sfby=1");
+    req.write("把 siubacy.top/提取地址的结果粘贴到此处(注意替换掉此提示文字)");
     req.end();
 }
 
 function detalCall(responseString) {
     console.log(responseString)
 };
-nodePostGetRequest("fxgl.jx.edu.cn", "443", "POST", detalCall, "/4136010895/studentQd/saveStu", "JSESSIONID=03D79F9A8DE8FA0900712639D3F84C3B; JSESSIONID=03D79F9A8DE8FA0900712639D3F84C3B");
+var cookies;
+nodePostGetRequest("fxgl.jx.edu.cn", "443", "POST", detalCall, "/4136010895/studentQd/saveStu", cookies);
